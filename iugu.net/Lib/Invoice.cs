d57bb6e695f64a8909576b3580912abb24661ec5
Rodrigo Couto <rscouto@gmail.com>
2016-01-29 06:08:15 -0200

    Torna parte de Invoice assíncrona

--- iugu.net/Lib/Invoice.cs
+++ iugu.net/Lib/Invoice.cs
@@ -17,7 +17,6 @@ namespace iugu.Lib
             BaseURI += "/invoices";
         }
 
-
         //limit (opcional)	Máximo de registros retornados
         //start (opcional)	Quantos registros pular do início da pesquisa (muito utilizado para paginação)
         //created_at_from (opcional)	Registros criados a partir desta data passada no parâmetro
@@ -29,13 +28,26 @@ namespace iugu.Lib
         public InvoiceListModel Get()
         {
             //TODO: Implementar GET com parametros
-            var retorno = GetAsync<InvoiceListModel>().Result;
+            var retorno = GetAsync().Result;
+            return retorno;
+        }
+
+        public async Task<InvoiceListModel> GetAsync()
+        {
+            //TODO: Implementar GET com parametros
+            var retorno = await GetAsync<InvoiceListModel>().ConfigureAwait(false);
             return retorno;
         }
 
         public InvoiceModel Get(string id)
         {
-            var retorno = GetAsync<InvoiceModel>(id).Result;
+            var retorno = GetAsync(id).Result;
+            return retorno;
+        }
+
+        public async Task<InvoiceModel> GetAsync(string id)
+        {
+            var retorno = await GetAsync<InvoiceModel>(id).ConfigureAwait(false);
             return retorno;
         }
 
@@ -62,6 +74,35 @@ namespace iugu.Lib
             string subscription_id = "", int credits = 0, Logs logs = null, CustomVariables custom_variables = null)
         {
             //TODO: implementar  custom_variables[]
+            var retorno = CreateAsync(email, due_date, items, return_url, expired_url, notification_url, tax_cents,
+                                      discount_cents, customer_id, ignore_due_email, subscription_id, credits, logs,
+                                      custom_variables).Result;
+            return retorno;
+        }
+
+        /// <summary>
+        /// Cria uma Fatura para um Cliente (Podendo ser um objeto cliente ou apenas um e-mail).
+        /// </summary>
+        /// <param name="email">E-Mail do cliente</param>
+        /// <param name="due_date">Data de Expiração (DD/MM/AAAA)</param>
+        /// <param name="items"> Itens da Fatura</param>
+        /// <param name="return_url">(opcional)	Cliente é redirecionado para essa URL após efetuar o pagamento da Fatura pela página de Fatura da Iugu</param>
+        /// <param name="expired_url">(opcional) Cliente é redirecionado para essa URL se a Fatura que estiver acessando estiver expirada</param>
+        /// <param name="notification_url">(opcional) URL chamada para todas as notificações de Fatura, assim como os webhooks (Gatilhos) são chamados</param>
+        /// <param name="tax_cents">(opcional) Valor dos Impostos em centavos</param>
+        /// <param name="discount_cents">(opcional)	Valor dos Descontos em centavos</param>
+        /// <param name="customer_id">(opcional) ID do Cliente</param>
+        /// <param name="ignore_due_email">(opcional) Booleano que ignora o envio do e-mail de cobrança</param>
+        /// <param name="subscription_id">(opcional) Amarra essa Fatura numa Assinatura</param>
+        /// <param name="credits">(opcional) Caso tenha o subscription_id, pode-se enviar o número de créditos a adicionar nessa Assinatura quando a Fatura for paga</param>
+        /// <param name="logs">(opcional) Logs da Fatura</param>
+        /// <param name="custom_variables">(opcional) Variáveis Personalizadas</param>
+        /// <returns></returns>
+        public async Task<InvoiceModel> CreateAsync(string email, DateTime due_date, Item[] items, string return_url = "",
+    string expired_url = "", string notification_url = "", int tax_cents = 0, int discount_cents = 0, string customer_id = "", bool ignore_due_email = false,
+    string subscription_id = "", int credits = 0, Logs logs = null, CustomVariables custom_variables = null)
+        {
+            //TODO: implementar  custom_variables[]
             var invoice = new
             {
                 email = email,
@@ -78,33 +119,58 @@ namespace iugu.Lib
                 logs = logs,
                 custom_variables = custom_variables
             };
-            var retorno = PostAsync<InvoiceModel>(invoice).Result;
+            var retorno = await PostAsync<InvoiceModel>(invoice).ConfigureAwait(false);
             return retorno;
         }
 
+
         public InvoiceModel Delete(string id)
         {
-            var retorno = DeleteAsync<InvoiceModel>(id).Result;
+            var retorno = DeleteAsync(id).Result;
+            return retorno;
+        }
+
+        public async Task<InvoiceModel> DeleteAsync(string id)
+        {
+            var retorno = await DeleteAsync<InvoiceModel>(id).ConfigureAwait(false);
             return retorno;
         }
 
         public InvoiceModel Put(string id, InvoiceModel model)
         {
-            var retorno = PutAsync<InvoiceModel>(id, model).Result;
+            var retorno = PutAsync(id, model).Result;
+            return retorno;
+        }
+
+        public async Task<InvoiceModel> PutAsync(string id, InvoiceModel model)
+        {
+            var retorno = await PutAsync<InvoiceModel>(id, model).ConfigureAwait(false);
             return retorno;
         }
 
         public InvoiceModel Refund(string id)
         {
-            BaseURI += string.Format("{0}/refund",id);
-            var retorno = PostAsync<InvoiceModel>(null).Result;
+            var retorno = RefundAsync(id).Result;
+            return retorno;
+        }
+
+        public async Task<InvoiceModel> RefundAsync(string id)
+        {
+            BaseURI += string.Format("{0}/refund", id);
+            var retorno = await PostAsync<InvoiceModel>(null).ConfigureAwait(false);
             return retorno;
         }
 
         public InvoiceModel Cancel(string id)
         {
+            var retorno = CancelAsync(id).Result;
+            return retorno;
+        }
+
+        public async Task<InvoiceModel> CancelAsync(string id)
+        {
             BaseURI += "/cancel";
-            var retorno = PutAsync<InvoiceModel>(id, null).Result;
+            var retorno = await PutAsync<InvoiceModel>(id, null).ConfigureAwait(false);
             return retorno;
         }
     }
