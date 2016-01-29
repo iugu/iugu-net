c6d1945fca3c714fc44192b0ed7cc64bbcb003d2
Rodrigo Couto <rscouto@gmail.com>
2016-01-29 07:31:07 -0200

    Torna parte de PaymentMethod assíncrona

--- iugu.net/Lib/PaymentMethod.cs
+++ iugu.net/Lib/PaymentMethod.cs
@@ -21,18 +21,30 @@ namespace iugu.Lib
         public PaymentMethod(string customerid)
         {
             _customerID = customerid;
-            BaseURI += string.Format("/customers/{0}/payment_methods",CustomerID);
+            BaseURI += string.Format("/customers/{0}/payment_methods", CustomerID);
         }
 
         public PaymentMethodModel Get()
         {
-            var retorno = GetAsync<PaymentMethodModel>().Result;
+            var retorno = GetAsync().Result;
+            return retorno;
+        }
+
+        public async Task<PaymentMethodModel> GetAsync()
+        {
+            var retorno = await GetAsync<PaymentMethodModel>().ConfigureAwait(false);
             return retorno;
         }
 
         public PaymentMethodModel Get(string id)
         {
-            var retorno = GetAsync<PaymentMethodModel>(id).Result;
+            var retorno = GetAsync(id).Result;
+            return retorno;
+        }
+
+        public async Task<PaymentMethodModel> GetAsync(string id)
+        {
+            var retorno = await GetAsync<PaymentMethodModel>(id).ConfigureAwait(false);
             return retorno;
         }
 
@@ -46,6 +58,20 @@ namespace iugu.Lib
         /// <param name="item_type">(opcional)	Transforma a forma de pagamento na padrão do cliente</param>
         public PaymentMethodModel Create(string description, CreditCard data, bool? set_as_default, string token = "", string item_type = "")
         {
+            var retorno = CreateAsync(description, data, set_as_default, token, item_type).Result;
+            return retorno;
+        }
+
+        /// <summary>
+        /// Cria uma Forma de Pagamento de Cliente.
+        /// </summary>
+        /// <param name="description">Descrição</param>
+        /// <param name="data">Dados da Forma de Pagamento</param>
+        /// <param name="set_as_default">(opcional)	Tipo da Forma de Pagamento. Atualmente suportamos apenas Cartão de Crédito (tipo credit_card). Só deve ser enviado caso não envie token.</param>
+        /// <param name="token">(opcional)	Token de Pagamento, pode ser utilizado em vez de enviar os dados da forma de pagamento</param>
+        /// <param name="item_type">(opcional)	Transforma a forma de pagamento na padrão do cliente</param>
+        public async Task<PaymentMethodModel> CreateAsync(string description, CreditCard data, bool? set_as_default, string token = "", string item_type = "")
+        {
             var paymentmethod = new
             {
                 description = description,
@@ -54,19 +80,31 @@ namespace iugu.Lib
                 token = token,
                 item_type = item_type
             };
-            var retorno = PostAsync<PaymentMethodModel>(paymentmethod).Result;
+            var retorno = await PostAsync<PaymentMethodModel>(paymentmethod).ConfigureAwait(false);
             return retorno;
         }
 
         public PaymentMethodModel Delete(string id)
         {
-            var retorno = DeleteAsync<PaymentMethodModel>(id).Result;
+            var retorno = DeleteAsync(id).Result;
+            return retorno;
+        }
+
+        public async Task<PaymentMethodModel> DeleteAsync(string id)
+        {
+            var retorno = await DeleteAsync<PaymentMethodModel>(id).ConfigureAwait(false);
             return retorno;
         }
 
         public PaymentMethodModel Put(string id, PaymentMethodModel model)
         {
-            var retorno = PutAsync<PaymentMethodModel>(id, model).Result;
+            var retorno = PutAsync(id, model).Result;
+            return retorno;
+        }
+
+        public async Task<PaymentMethodModel> PutAsync(string id, PaymentMethodModel model)
+        {
+            var retorno = await PutAsync<PaymentMethodModel>(id, model).ConfigureAwait(false);
             return retorno;
         }
     }
