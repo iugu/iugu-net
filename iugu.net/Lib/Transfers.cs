6d7d18ee2e3673873fc7dfc1c49d4c2855df5e5c
Rodrigo Couto <rscouto@gmail.com>
2016-01-29 06:38:20 -0200

    Torna parte de Transfers assíncrona

--- iugu.net/Lib/Transfers.cs
+++ iugu.net/Lib/Transfers.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace iugu.Lib
 {
-    class Transfers: APIResource
+    class Transfers : APIResource
     {
         public Transfers()
         {
@@ -15,7 +15,13 @@ namespace iugu.Lib
 
         public TransferModel Get()
         {
-            var retorno = GetAsync<TransferModel>().Result;
+            var retorno = GetAsync().Result;
+            return retorno;
+        }
+
+        public async Task<TransferModel> GetAsync()
+        {
+            var retorno = await GetAsync<TransferModel>().ConfigureAwait(false);
             return retorno;
         }
 
@@ -27,12 +33,24 @@ namespace iugu.Lib
         /// <returns></returns>
         public TransferModel Create(string receiver_id, int amount_cents)
         {
+            var retorno = CreateAsync(receiver_id, amount_cents).Result;
+            return retorno;
+        }
+
+        /// <summary>
+        /// Transfere um determinado valor de sua conta para a conta destino.
+        /// </summary>
+        /// <param name="receiver_id">Account ID da conta que irá receber o valor</param>
+        /// <param name="amount_cents">Valor a transferir em centavos</param>
+        /// <returns></returns>
+        public async Task<TransferModel> CreateAsync(string receiver_id, int amount_cents)
+        {
             var user = new
             {
                 receiver_id = receiver_id,
                 amount_cents = amount_cents
             };
-            var retorno = PostAsync<TransferModel>(user).Result;
+            var retorno = await PostAsync<TransferModel>(user).ConfigureAwait(false);
             return retorno;
         }
     }
