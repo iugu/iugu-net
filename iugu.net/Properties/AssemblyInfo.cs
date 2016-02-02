4cfbae601cca704a8f47c531dd10e6039c03f385
Rodrigo Couto <rscouto@gmail.com>
2016-02-01 23:22:21 -0200

    Integra com CI appveyor para build e deploy
    * Atualiza informações do AssembyInfo

--- iugu.net/Properties/AssemblyInfo.cs
+++ iugu.net/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("iugu.net")]
 [assembly: AssemblyCopyright("IUGU Copyright ©  2016")]
 [assembly: AssemblyTrademark("IUGU")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
