c0c8c39fca4880d671989775425b27a67553893f
Rodrigo Couto <rscouto@gmail.com>
2016-02-02 07:44:55 -0200

    Implementa Api de PaymentToken
    Fix #14

---
+++ iugu.net/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iugu.net
+{
+    public static class Constants
+    {
+        public static class PaymentMethod
+        {
+            public const string CREDIT_CARD = "credit_card";
+            public const string BANK_SLIP = "bank_slip";
+        }
+    }
+}
