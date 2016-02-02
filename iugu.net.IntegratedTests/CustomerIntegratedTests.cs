f511f23566f9b987a14a74e66949727d05f74163
Rodrigo Couto <rscouto@gmail.com>
2016-02-02 15:53:44 -0200

    Amplia testes integrados para api de customers
    Ref #16

--- iugu.net.IntegratedTests/CustomerIntegratedTests.cs
+++ iugu.net.IntegratedTests/CustomerIntegratedTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace iugu.UnitTest
 {
@@ -9,14 +10,55 @@ namespace iugu.UnitTest
     public class CustomerIntegratedTests
     {
         [Test]
-        public void Create_a_customer_with_success()
+        public async Task Create_a_customer_with_success()
         {
+            // Arrange
             var custom = new List<CustomVariables>();
             custom.Add(new CustomVariables { name = "Tipo", value = "Desmanche" });
             custom.Add(new CustomVariables { name = "Representante", value = "Fabio Munhoz (RJ)" });
 
-            var usuario = new Lib.Customer().Create("malka2@gmail.com", "Daniel Teste 2 C#", "teste da api em C#", custom);
-            var criado = usuario.id;
+            CustomerModel myClient;
+
+            // Act
+            using (var apiClient = new Lib.Customer())
+            {
+                myClient = await apiClient.CreateAsync("malka2@gmail.com", "Daniel Teste 2 C#", "teste da api em C#", custom).ConfigureAwait(false);
+            };
+
+            // Assert
+            Assert.That(myClient.email, Is.EqualTo("malka2@gmail.com"));
+            Assert.That(myClient.id, Is.Not.Empty);
+        }
+
+        [Test]
+        public void Search_a_not_found_customer()
+        {
+            // Arrange
+            CustomerModel myClient;
+
+            // Act
+            using (var apiClient = new Lib.Customer())
+            {
+                Assert.Throws<AggregateException>(() => apiClient.Get(Guid.NewGuid().ToString()), "Not Found");
+            };
+
+        }
+
+        [Test]
+        public async Task List_customers_with_success()
+        {
+            // Arrange
+            CustomersModel myClients;
+
+            // Act
+            using (var apiClient = new Lib.Customer())
+            {
+                myClients = await apiClient.GetAsync().ConfigureAwait(false);
+            };
+
+            // Assert
+            Assert.That(myClients, Is.Not.Null);
+            Assert.That(myClients.items, Is.Not.Empty);
         }
     }
 }
