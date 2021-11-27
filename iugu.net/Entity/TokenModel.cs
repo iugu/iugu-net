248a43d98e1aedc190746c48bd70ae1ce538005e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-27 22:27:58 +0800

    Delete iugu.net directory

--- iugu.net/Entity/TokenModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace iugu.net.Entity
-{
-    // TODO: Precisa de refatoração, nomes fora do padrão .Net, sem documentação também
-    public class TokenModel
-    {
-        public string id { get; set; }
-        public string method { get; set; }
-    }
-}
